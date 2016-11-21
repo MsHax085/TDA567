7f32557e63f51a618d3e724f9733465ea3f40fd8
SamtherealMVP <victor.christoffersson@telia.com>
2016-11-21 18:20:48 +0100

    Files for report
    Run WorkScheduleTestSuite to run both test classes for A) and B)

---
+++ WorkScheduleTestSuite.java
@@ -0,0 +1,12 @@
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+	@RunWith(Suite.class)
+	@Suite.SuiteClasses({
+		TestClass_v2.class,
+		TestClass2.class
+	})
+	public class WorkScheduleTestSuite {
+	// nothing goes here	 
+	}
